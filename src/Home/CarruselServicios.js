a13e3678e4d9ccf9792b62853fda1e20c8faaa29
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-06-28 23:00:02 -0300

    Update CarruselServicios.js

--- src/Home/CarruselServicios.js
+++ src/Home/CarruselServicios.js
@@ -1,48 +1,56 @@
-import {Carousel} from '3d-react-carousal';
+import { Carousel } from '3d-react-carousal';
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-function CarruselServicios(){
+function CarruselServicios() {
   let slides = [
     <Card style={{ width: '50%' }} alt="1">
-      <Card.Img variant="top" src="https://picsum.photos/800/301/?random" />
+      <Card.Img variant="top" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <p>Talleres grupales de locución,
+            neutro, oratoria, radio, podcast.</p>
+          <p>Clases personalizadas de:
+            locución, oratoria, radio, neutro
+            y podcast. </p>
+          <p style={{ visibility: 'hidden' }}>Asesorías Únicas</p>
+          <p style={{ visibility: 'hidden' }}>Asesorías Únicas</p>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>,
-     <Card style={{ width: '50%' }}  alt="2">
-     <Card.Img variant="top" src="https://picsum.photos/800/301/?random" />
-     <Card.Body>
-       <Card.Title>Card Title</Card.Title>
-       <Card.Text>
-         Some quick example text to build on the card title and make up the
-         bulk of the card's content.
-       </Card.Text>
-       <Button variant="primary">Go somewhere</Button>
-     </Card.Body>
-   </Card>,
-     <Card  style={{ width: '50%' }} alt="3">
-     <Card.Img variant="top" src="https://picsum.photos/800/301/?random" />
-     <Card.Body>
-       <Card.Title>Card Title</Card.Title>
-       <Card.Text>
-         Some quick example text to build on the card title and make up the
-         bulk of the card's content.
-       </Card.Text>
-       <Button variant="primary">Go somewhere</Button>
-     </Card.Body>
-   </Card>,
+    <Card style={{ width: '50%' }} alt="1">
+      <Card.Img variant="top" />
+      <Card.Body>
+        <Card.Text>
+          <p>Talleres disruptivos para
+            empresas: locución, radioteatro,
+            radio y podcast.</p>
+          <p>Workshop para empresas:
+            locución, oratoria, neutro. </p>
+          <p>Asesorías Únicas</p>
+          <p style={{ visibility: 'hidden' }}>Asesorías Únicas</p>
 
+        </Card.Text>
+      </Card.Body>
+    </Card>,
+    <Card style={{ width: '50%' }} alt="1">
+      <Card.Img variant="top" />
+      <Card.Body>
+        <Card.Text>
+          <p>Locuciones comerciales castellano
+            neutro y rioplatense</p>
+          <p>Grabaciones en homestudio
+            propio.</p>
+          <p>Conducción de eventos</p>
+          <p>Radio, Podcast y TV</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>,
   ];
-  return <Carousel slides={slides} autoplay={true} interval={10000} />;
+  return <div><Carousel slides={slides} autoplay={true} interval={10000} /></div>;
 }
 
 export default CarruselServicios;
 
 
-    
+
