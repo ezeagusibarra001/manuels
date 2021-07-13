ac7a0e6a841a7e0e14cb8c07108f3fa4773ebc89
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-07-13 00:47:34 -0300

    MANUELS 12/7

--- src/Clasesonline/ClasesGrupales.js
+++ src/Clasesonline/ClasesGrupales.js
@@ -7,7 +7,150 @@ function ClasesGrupales() {
     return(
 
         <div className="ContainerClasesGrupales">
-
+            <div className="SubContainerClasesGrupales">
+                <div className="ContainerHijo1ClasesGrupales">
+                    <div className="ContainerTitulosClasesGrupales">
+                        <h1 className="TituloClasesGrupales">Clases Grupales</h1>
+                        <p  className="SubTituloClasesGrupales">Las Clases Grupales pueden ser de:</p>
+                    </div>
+                    <div className="ContainerItem1Grupales">
+                        <div className="ContainerIconoGrupales1">
+                            <img
+                            className="Icono1clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem1Grupales">
+                            <h6>LOCUCIÓN (NIVEL 1 Y 2)</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem2Grupales">
+                        <div className="ContainerIconoGrupales2">
+                            <img
+                            className="Icono2clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem2Grupales">
+                            <h6>LOCUCIÓN COMERCIAL</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem3Grupales">
+                        <div className="ContainerIconoGrupales3">
+                            <img
+                            className="Icono3clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem3Grupales">
+                            <h6>RADIO</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem4Grupales">
+                        <div className="ContainerIconoGrupales4">
+                            <img
+                            className="Icono4clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem4Grupales">
+                            <h6>PODCAST</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem5Grupales">
+                        <div className="ContainerIconoGrupales5">
+                            <img
+                            className="Icono5clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem5Grupales">
+                            <h6>RADIO TEATRO</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem6Grupales">
+                        <div className="ContainerIconoGrupales6">
+                            <img
+                            className="Icono6clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem6Grupales">
+                            <h6>NEUTRO</h6>
+                        </div>
+                    </div>
+                    <div className="ContainerItem7Grupales">
+                        <div className="ContainerIconoGrupales7">
+                            <img
+                            className="Icono7clasesGrupales"
+                            src="./assets/iconoc9.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem7Grupales">
+                            <h6>ORATORIA</h6>
+                        </div>
+                    </div>
+                </div>
+                <div className="ContainerHijo2ClasesGrupales">
+                    <div className="ContainerItem8Grupales">
+                        <div className="ContainerIconoGrupales8">
+                            <img
+                            className="Icono8clasesGrupales"
+                            src="./assets/iconoc6.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem8Grupales">
+                            <p>
+                                Son talleres sincrónicos, virtuales por la plataforma Skype de grupos reducidos, 
+                                donde la práctica es el pilar fundamental de cada encuentro
+                            </p>
+                        </div>
+                    </div>
+                    <div className="ContainerItem9Grupales">
+                        <div className="ContainerIconoGrupales9">
+                            <img
+                            className="Icono9clasesGrupales"
+                            src="./assets/iconoc4.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem9Grupales">
+                            <p>
+                                Cada taller tiene entre 4 y 8 encuentros dependiendo cuál sea.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="ContainerItem10Grupales">
+                        <div className="ContainerIconoGrupales10">
+                            <img
+                            className="Icono10clasesGrupales"
+                            src="./assets/iconoc7.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem10Grupales">
+                            <p>
+                                Se entrega material de práctica y certificado de finalización y asistencia
+                            </p>
+                        </div>
+                    </div>
+                    <div className="ContainerItem11Grupales">
+                        <div className="ContainerIconoGrupales11">
+                            <img
+                            className="Icono11clasesGrupales"
+                            src="./assets/iconoc8.png"
+                            />
+                        </div>
+                        <div className="ContainerTextoItem11Grupales">
+                            <p>
+                                Si queres ser el primero en enterarte de proximos talleres grupales o necesitas 
+                                hacer una consulta
+                            </p>
+                        </div>
+                    </div>
+                    <div className="ContainerButtonGrupales">
+                        <button className="ButtonGrupales" > <b>¡QUIERO SABER MÁS!</b> </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
     
