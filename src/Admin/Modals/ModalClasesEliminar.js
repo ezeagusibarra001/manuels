f9cde709e77d318b815e16f3e5b5406fa8c4bf58
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-07-22 10:09:24 -0300

    ABM CLASES COMPLETO

--- src/Admin/Modals/ModalClasesEliminar.js
+++ src/Admin/Modals/ModalClasesEliminar.js
@@ -7,20 +7,20 @@ import "../Admin.css"
 import { useToasts } from "react-toast-notifications";
 function ModalClasesEliminar(props) {
     const { addToast } = useToasts();
-    const { obtenerClases, clases} = useHome()
+    const { obtenerClases, clases } = useHome()
     const { showEliminar, setShowEliminar } = props;
     const handleClose = () => {
         setShowEliminar(false)
     }
-    const handleEliminar= async (clases) => {
+    const handleEliminar = async (clases) => {
         await clienteAxios
-            .delete(`/lessons/${clases.id}`)
+            .delete(`/lessons/${clases.idLesson}`)
             .then((res) => {
                 console.log(res.data);
                 obtenerClases()
             })
             .catch((err) => {
-                console.log("error put", err);
+                console.log("error delete", err);
             });
     };
     return (
@@ -36,14 +36,19 @@ function ModalClasesEliminar(props) {
                     <Modal.Title id="modal-tittle">Eliminar Clase</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                        <select aria-label="Default select example">
-                        {clases.map((clases)=>(
-                            <option value={clases.id}>{clases.title}</option>
+                    <ul aria-label="Default select example">
+                        {clases.map((clase) => (
+                            <div>
+                                <li>{clase.title}</li>
+                                <Button
+                                    variant="primary"
+                                    onClick={() => handleEliminar(clase)}
+                                >Borrar</Button>
+                            </div>
                         ))}
-                        </select>
+                    </ul>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="primary" onClick={()=>handleEliminar(clases)}>Eliminar</Button>
                     <Button variant="primary" onClick={handleClose}>Cerrar</Button>
 
                 </Modal.Footer>
