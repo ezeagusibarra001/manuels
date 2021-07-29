ab1d97ac46fa07b8fc0e83cb5d70b54cc088a684
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-07-29 20:26:04 -0300

    MANUELS 29/7

--- src/Home/Feedback.js
+++ src/Home/Feedback.js
@@ -8,14 +8,27 @@ function Feedback(props) {
         {
             reseña:"Desafie todos mis miedos , me supere a mi misma utilizando el poder de mi voz. Gracias Manuels por haberme ayudado a alcanzar una mejor version de mi misma!!!",
             author:"Petosa Ayala Franco",
+            validator:true,
         },
         {
             reseña:"Desafie todos mis miedos , me supere a mi misma utilizando el poder de mi voz Gracias Manuels por haberme ayudado a alcanzar una mejor version de mi misma!!! Hoy me siento mas influencer que nunca.A seguir creciendo",
-            author:"Ibarra Ezeuiel Agustin"
+            author:"Ibarra Ezeuiel Agustin",
+            validator:true,
         },
         {
             reseña:"Desafie todos mis miedos , me supere a mi misma utilizando el poder de mi voz Gracias Manuels por haberme ayudado a alcanzar una mejor version de mi misma!!! Hoy me siento mas influencer que nunca.A seguir creciendo. Super interesante y enriquecedor.",
-            author:"Oubiña Maria Pilar"
+            author:"Oubiña Maria Pilar",
+            validator:true,
+        },
+        {
+            reseña:"Me encanta la pija",
+            author:"Centu",
+            validator:false,
+        },
+        {
+            reseña:"Mi amor fue culpa de tiffer puedo jurarlo",
+            author:"Joaquin Jimenez Jaureche",
+            validator:true,
         },
     ]
 
@@ -28,7 +41,7 @@ function Feedback(props) {
                 </div>
                 <div className="ContainerFeedbackCarrusel">
                     <Carousel className="FeedbackCarrusel">
-                        {feedbacks.map((feedbacks) =>(
+                        {feedbacks.filter((feedbacks) =>feedbacks.validator ===true).map(feedbacks=>
                             <Carousel.Item className="ContainerFeedbackCarruselSection">
                                 <div className="ContainerCarruselReseñas">
                                     <h3 className="FeedbackReseña">
@@ -37,7 +50,7 @@ function Feedback(props) {
                                     <h1 className="FeedbackAuthor">-{feedbacks.author}</h1>
                                 </div>
                             </Carousel.Item>
-                        ))}
+                        )}
                     </Carousel>
                 </div>
             </div>
