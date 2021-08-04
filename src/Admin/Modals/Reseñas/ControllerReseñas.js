e21dce124f8b70c20ddecf9490d913fe1ca9d4e2
Ezequiel Agustin Ibarra <ibarra.ezequiel01@gmail.com>
2021-08-04 16:37:00 -0300

    ABM RESEÑAS DONE

--- src/Admin/Modals/Reseñas/ControllerReseñas.js
+++ src/Admin/Modals/Reseñas/ControllerReseñas.js
@@ -1,46 +1,74 @@
 import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import Card from 'react-bootstrap/Card'
-/*import clienteAxios from '../../../config/clienteAxios'*/
-import {useHome} from '../../../context/home-context'
+import clienteAxios from '../../../config/clienteAxios'
+import { useHome } from '../../../context/home-context'
 import "../../Admin.css"
 import { useToasts } from "react-toast-notifications";
-import Feedback from '../../../Home/Feedback'
+import { Card } from 'react-bootstrap'
+
 
 
 function Controllerreseñas(props) {
-
-    const {feedbacks, setFeedbacks} = useHome()
-    
-    const validation= ()=>{
-
-    }
-/*-----------------------------------------------------------------------------*/
-    const showVerReseñas= props.showVerReseñas
-    const setShowVerReseñas= props.setShowVerReseñas
+    const { addToast } = useToasts();
+    const { reviews, axiosConfig, obtenerReseñas } = useHome()
+    const handleEliminar = async (review) => {
+        await clienteAxios
+            .delete(`/reviews/${review.idReview}`, axiosConfig)
+            .then((res) => {
+                console.log("Reseña Eliminada");
+                obtenerReseñas()
+            })
+            .catch((err) => {
+                console.log("error delete", err);
+            });
+    };
+    const handleModificar = async (review) => {
+        await clienteAxios
+            .put(`/reviews/accept/${review.idReview}`, {}, axiosConfig)
+            .then((res) => {
+                console.log("Reseña Actualizada", res.data);
+                addToast("Reseña Live! :)", {
+                    appearance: "success",
+                    autoDismiss: true,
+                });
+                obtenerReseñas();
+            })
+            .catch((err) => {
+                console.log("error put", err);
+                console.log(review.idReview)
+                console.log(axiosConfig)
+            });
+    };
+    /*-----------------------------------------------------------------------------*/
+    const showVerReseñas = props.showVerReseñas
+    const setShowVerReseñas = props.setShowVerReseñas
     const handleCloseVerReseñas = () => setShowVerReseñas(false);
-/*-----------------------------------------------------------------------------*/
-    return(
-        <div> 
+    /*-----------------------------------------------------------------------------*/
+    return (
+        <div>
             <Modal show={showVerReseñas} onHide={handleCloseVerReseñas}>
                 <Modal.Header closeButton />
-                {feedbacks.map((feedbacks) => (
+                {reviews.filter((reviews) =>reviews.validate === false).map((review) => (
                     <Modal.Body>
-                        <textarea 
-                        value={feedbacks.author}
-                        />
-                        <textarea 
-                        value={feedbacks.reseña}
-                        />
-                        <button  onClick={validation}>Aceptar</button>
-                        <button>Eliminar</button>
+                        <Card>
+                            <label>{review.name}</label>
+                            <label>{review.commentary}</label>
+                            <Button
+                                variant="primary"
+                                onClick={() => handleEliminar(review)}
+                            >Borrar</Button>
+                            <Button
+                                variant="primary"
+                                onClick={() => handleModificar(review)}
+                            >Aceptar</Button>
+                        </Card>
+
                     </Modal.Body>
                 ))}
                 <Modal.Footer>
                     <Button onClick={handleCloseVerReseñas}>Close</Button>
-                    <Button  onClick={handleCloseVerReseñas}>Save Changes</Button>
+                    <Button onClick={handleCloseVerReseñas}>Save Changes</Button>
                 </Modal.Footer>
             </Modal>
         </div>
