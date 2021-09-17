4471369830c1ec607196a5460671eeaa59b8db24
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-09-16 22:51:42 -0300

    Carrusel

--- src/Home/CarruselMarcas.js
+++ src/Home/CarruselMarcas.js
@@ -62,15 +62,9 @@ function CarruselMarcas(){
                     </div>)}
                 </div>
                 <div className="Controllers">
-                    <button className="ControllerButton" onClick={back}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="Arrow"  class="bi bi-caret-left-fill" viewBox="0 0 16 16">
-                            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
-                        </svg>
+                    <button className="ControllerButtonLeft" onClick={back}>
                     </button>
-                    <button className="ControllerButton" onClick={foward}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="Arrow"  class="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg>
+                    <button className="ControllerButtonRight" onClick={foward}>
                     </button>
                 </div>
             </div>
