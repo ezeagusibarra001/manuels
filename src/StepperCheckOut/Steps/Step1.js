23626b5363f10ea6d8db83e3e28c3ae74fa1f3ea
ezeibarra <ezequiel.ibarra@garajedeideas.com>
2021-09-22 19:41:07 -0300

    Update Step1.js

--- src/StepperCheckOut/Steps/Step1.js
+++ src/StepperCheckOut/Steps/Step1.js
@@ -1,49 +1,54 @@
 import React from "react";
-import '../cssSteps/step1.css'
+import "../cssSteps/step1.css";
 
 function Step1() {
   return (
-    <div>
+    <div className="container">
       <h1 className="Checkouttitle">Completar los siguientes datos:</h1>
-      <input
-        className="Checkoutinput"
-        type="text"
-        name="name"
-        placeholder="Nombre"
-        //style={Styles}
-        maxLength="15"
-        //onChange={handleChange}
-      />
 
-      <input
-        className="Checkoutinput"
-        type="text"
-        name="lastname"
-        placeholder="Apellido"
-        //style={Styles}
-        maxLength="15"
-        //onChange={handleChange}
-      />
+      <div className="row">
+        <div>
+          <input
+            className="Checkoutinput"
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            //style={Styles}
+            maxLength="15"
+            //onChange={handleChange}
+          />
 
-      <input
-        className="Checkoutinput"
-        type="number"
-        name="phone"
-        placeholder="Telefono"
-        //style={Styles}
-        //onChange={handleChange}
-        maxLength="15"
-      />
+          <input
+            className="Checkoutinput"
+            type="text"
+            name="lastname"
+            placeholder="Apellido"
+            //style={Styles}
+            maxLength="15"
+            //onChange={handleChange}
+          />
 
-      <input
-        className="Checkoutinput"
-        type="e-mail"
-        name="email"
-        placeholder="E-mail"
-        //style={Styles}
-        maxLength="50"
-        //onChange={handleChange}
-      />
+          <input
+            className="Checkoutinput"
+            type="number"
+            name="phone"
+            placeholder="Telefono"
+            //style={Styles}
+            //onChange={handleChange}
+            maxLength="15"
+          />
+
+          <input
+            className="Checkoutinput"
+            type="e-mail"
+            name="email"
+            placeholder="E-mail"
+            //style={Styles}
+            maxLength="50"
+            //onChange={handleChange}
+          />
+        </div>
+      </div>
     </div>
   );
 }
