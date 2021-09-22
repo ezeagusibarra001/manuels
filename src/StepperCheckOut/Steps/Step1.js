fa8b4380d1153d2490e49d064aab4a2fdc818667
ezeibarra <ezequiel.ibarra@garajedeideas.com>
2021-09-22 18:57:02 -0300

    Update Step1.js

--- src/StepperCheckOut/Steps/Step1.js
+++ src/StepperCheckOut/Steps/Step1.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
 
 function Step1() {
-    return (
-        <div>
-            Step 1
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="Checkouttitle">Completar los siguientes datos:</h1>
+      <input
+        className="Checkoutinput"
+        type="text"
+        name="name"
+        placeholder="Nombre"
+        //style={Styles}
+        maxLength="15"
+        //onChange={handleChange}
+      />
+
+      <input
+        className="Checkoutinput"
+        type="text"
+        name="lastname"
+        placeholder="Apellido"
+        //style={Styles}
+        maxLength="15"
+        //onChange={handleChange}
+      />
+
+      <input
+        className="Checkoutinput"
+        type="number"
+        name="phone"
+        placeholder="Telefono"
+        //style={Styles}
+        //onChange={handleChange}
+        maxLength="15"
+      />
+
+      <input
+        className="Checkoutinput"
+        type="e-mail"
+        name="email"
+        placeholder="E-mail"
+        //style={Styles}
+        maxLength="50"
+        //onChange={handleChange}
+      />
+    </div>
+  );
 }
 
-export default Step1
+export default Step1;
