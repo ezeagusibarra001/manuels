08b2228281dd205ac1f72b457c6c48d4b8dc0509
FrancoPetosa99 <franco.petosa15@gmail.com>
2021-11-25 19:29:51 -0300

    Manuels

--- src/Nabvar/Nabvar.js
+++ src/Nabvar/Nabvar.js
@@ -3,8 +3,10 @@ import NabvarDesktop from "./NabvarDesktop"
 import NabvarMobile from "./NabvarMobile"
 import {scroller} from "react-scroll";
 import NabvarAdmin from "./NabvarAdmin";
+import { useHistory } from "react-router-dom";
 function Nabvar(props) {
     const [screenSize,setScreenSize]=useState(window.innerWidth)
+    const history = useHistory();
     const screenChange =()=>{
         setScreenSize(window.innerWidth)
         console.log("Screen size:",screenSize)
@@ -13,27 +15,33 @@ function Nabvar(props) {
     window.addEventListener("load",()=>{screenChange();})   //SE EJECUTA CADA VEZ QUE LA PAGINA SE RECARGA
 /*-------------OPTIMIZAMOS EL CODIGO ENVIANDO A TRAVES DE LAS PROPS LOS METODOS DE SCROLL A LOS COMPONNETES MOBILE Y DESKTOP-------------*/
     const ScrollServicios=()=>{
-        scroller.scrollTo('Servicios',{
-            smooth: true,
-            offset: -70,
-            duration:2500,
-        })
+        history.push("/");
+        setTimeout(function(){
+            scroller.scrollTo('Servicios',{
+                smooth: true,
+                offset: -70,
+                duration:2500,
+            })
+          },500);
     }
     const ScrollExperiencia=()=>{
-        scroller.scrollTo('Experiencia',{
-            smooth: true,
-            offset: -70,
-            duration:2000,
-        })
+        history.push("/");
+        setTimeout(function(){
+            scroller.scrollTo('Servicios',{
+                smooth: true,
+                offset: -70,
+                duration:2500,
+            })
+        },500);
     } 
 /*-----------------------------------------------------------------------------------------------------------------------------------------*/
     if(localStorage.getItem("login")){ //EVALUAMOS EL VALOR DE LA VARIABLE GUARDADA EN EL STORAGE DEL NAVEGADOR
         return(<NabvarAdmin/>)
     }else{
         if(screenSize>700){
-            return(<NabvarDesktop ScrollServicios={ScrollServicios} ScrollExperiencia={ScrollExperiencia} />)
+            return(<NabvarDesktop ScrollServicios={ScrollServicios} ScrollExperiencia={ScrollExperiencia}  />)
         }
-        else{return(<NabvarMobile ScrollServicios={ScrollServicios} ScrollExperiencia={ScrollExperiencia}/>)}
+        else{return(<NabvarMobile ScrollServicios={ScrollServicios} ScrollExperiencia={ScrollExperiencia}  />)}
     }  
 }
 export default Nabvar;
